54276d4e433d7468fd2170e70750400858a1664d
Ben Rowe <ben.rowe.83@gmail.com>
2018-03-28 16:07:34 +1100

    WIP - webhook controller

--- src/ConsoleToWebhookHandler.php
+++ src/ConsoleToWebhookHandler.php
@@ -18,9 +18,21 @@ class ConsoleToWebhookHandler
         // identify which mapping is requested by the url structure
         // /{endpoint}
         // or x-webhook-endpoint: {endpoint}
+        $binding = $this->identifyBinding($request);
+        $validToken = (new ValidateBindingToken($binding->token))->fromRequest($request)->isValid();
+        if (!$validToken) {
+            $mapping->run($app);
+        }
 
         // verify the token (?token={token}, $_POST['token'], x-webhook-token: {token})
         var_dump($request);
+    }
+
+    private function identifyBinding($request)
+    {
+        // get webhook -> bindings
+        // itereate through
+
     }
 
 
