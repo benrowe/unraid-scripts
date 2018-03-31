f37368c9252f6061a9602adb29f74f1e90451263
Ben Rowe <ben.rowe.83@gmail.com>
2018-03-31 17:25:28 +1100

    Add sampler handler for test logic

--- src/Command/TestCommand.php
+++ src/Command/TestCommand.php
@@ -5,6 +5,7 @@ namespace App\Command;
 
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\ArrayInput;
 
 class TestCommand extends \Symfony\Component\Console\Command\Command
 {
@@ -17,5 +18,12 @@ class TestCommand extends \Symfony\Component\Console\Command\Command
     public function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln('Hello, World');
+    }
+
+    public static function handleRequest()
+    {
+        return new ArrayInput([
+            'command' => 'hello:world'
+        ]);
     }
 }
