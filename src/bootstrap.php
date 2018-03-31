95849ccf6142a25456619e003418d71fb5eae98e
Ben Rowe <ben.rowe.83@gmail.com>
2018-03-31 17:23:56 +1100

    Add .env/config loading to bootstrap

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,13 +1,25 @@
 <?php
 
 use App\Console;
+use Noodlehaus\Config;
 
-require_once realpath(__DIR__.'/../').'/vendor/autoload.php';
+$rootDir = realpath(__DIR__.'/../');
+require_once $rootDir.'/vendor/autoload.php';
+
+// env variables
+if (file_exists($rootDir.'/data/.env')) {
+    $dotenv = new Dotenv\Dotenv($rootDir.'/data/');
+    $dotenv->load();
+}
 
 $application = new Console('Unraid Scripts', '1.0');
 
 // detect and load console commands from the App\Command namespace
 Console::load($application, 'App\Command', 'src/Command');
 Console::load($application, 'App\Commands', 'data/commands');
+
+// load application config
+$config = new Config($rootDir.'/data/config.yml');
+$application->setConfig($config);
 
 return $application;
