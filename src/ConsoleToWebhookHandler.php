5a6a73258b4c4dec470847b6e40b89c9d95baa60
Ben Rowe <ben.rowe.83@gmail.com>
2018-03-31 17:25:11 +1100

    Complete webhook logic

--- src/ConsoleToWebhookHandler.php
+++ src/ConsoleToWebhookHandler.php
@@ -2,11 +2,15 @@
 
 namespace App;
 
+use Noodlehaus\Config;
 use Symfony\Component\HttpFoundation\Request;
+use Teapot\StatusCode;
+use Teapot\HttpException;
 
 class ConsoleToWebhookHandler
 {
     private $app;
+    private $config;
 
     public function __construct(Console $app)
     {
@@ -19,21 +23,52 @@ class ConsoleToWebhookHandler
         // /{endpoint}
         // or x-webhook-endpoint: {endpoint}
         $binding = $this->identifyBinding($request);
-        $validToken = (new ValidateBindingToken($binding->token))->fromRequest($request)->isValid();
-        if (!$validToken) {
-            $mapping->run($app);
+        if (!$binding) {
+            throw new HttpException('Unknown webhook', StatusCode::NOT_FOUND);
         }
 
-        // verify the token (?token={token}, $_POST['token'], x-webhook-token: {token})
-        var_dump($request);
+        $validToken = (new ValidateBindingToken((string)$binding->token))->isValid($request);
+        if (!$validToken) {
+            throw new HttpException('Invalid authentication', StatusCode::FORBIDDEN);
+        }
+        (new AppBinder($this->app, $request, $binding))->run();
     }
 
-    private function identifyBinding($request)
+    private function identifyBinding(Request $request): ?Binding
     {
-        // get webhook -> bindings
-        // itereate through
+        $bindings = $this->getConfig()->get('webhooks.bindings');
 
+        foreach ($bindings as $bind) {
+            if ($this->matchBinding($bind['id'], $request)) {
+                return $this->makeBinding($bind);
+                break;
+            }
+        }
+        return null;
     }
 
+    /**
+     * Check to see if the route or x-webhook-endpoint header matches the supplied id
+     */
+    private function matchBinding(string $id, Request $request): bool
+    {
+        $id = strtolower($id);
+        return
+            strtolower((string)$request->headers->get('x-webhook-endpoint')) === $id ||
+            strtolower(ltrim($request->getPathInfo(), '/')) === $id;
+    }
 
+    private function makeBinding(array $binding): Binding
+    {
+        $obj = new Binding;
+        $obj->id = $binding['id'];
+        $obj->token = $binding['token'];
+        $obj->commands = $binding['commands'];
+        return $obj;
+    }
+
+    private function getConfig(): Config
+    {
+        return $this->app->getConfig();
+    }
 }
